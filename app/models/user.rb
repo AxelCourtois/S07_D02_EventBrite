0c6dfca036848950063a4cb6e75ece4a39cde1e1
COURTOIS <courtois.a@hotmail.com>
2023-11-07 11:30:05 +0100

    ajout usermailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
