4bfe202ee02049bb4b81a9c45adde71c7ecbfb08
COURTOIS <courtois.a@hotmail.com>
2023-11-07 12:11:58 +0100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
   has_many :events, through: :attendances
   has_many :attendances
 
-  after_create :welcome_send
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
