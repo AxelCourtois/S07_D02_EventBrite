0c55cad98b9d2c89d83ee4a407dab9f2fe691aee
COURTOIS <courtois.a@hotmail.com>
2023-11-07 11:44:18 +0100

    on enlève les validate de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   has_many :events, through: :attendances
   has_many :attendances
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :description, presence: true
-
   after_create :welcome_send
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
