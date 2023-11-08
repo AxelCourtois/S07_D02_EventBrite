5b3588c3c912bbefc819ae43905ed893f28d0cb2
COURTOIS <courtois.a@hotmail.com>
2023-11-08 03:59:26 +0100

    l'ID

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILJET_LOGIN'],
   :password => ENV['MAILJET_PWD'],
-  :domain => 'eventbrite-de-fou.herokuapp.com',
+  :domain => 'eventbrite-de-fou-034d4b4516f4.herokuapp.com',
   :address => 'in-v3.mailjet.com',
   :port => 587,
   :authentication => :plain,
